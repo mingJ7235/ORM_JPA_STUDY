54b9a56070ad4ce5a1534ec36778fb02b698d3db
김민재 <joshuara7235@gmail.com>
2021-08-01 20:02:59 +0900

    Practice basic -> RDB focus design

---
+++ jpashopPractice/src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop;
+
+import jpabook.jpashop.domain.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+
+        try {
+            tx.commit(); //이때 쿼리가 날아감
+        } catch (Exception e) {
+            tx.rollback();;
+        } finally {
+            em.close();
+        }
+
+
+        emf.close();
+    }
+}
