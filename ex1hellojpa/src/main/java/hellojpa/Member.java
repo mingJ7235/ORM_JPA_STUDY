e229cae048020e340907b1876cdd69c510328569
김민재 <joshuara7235@gmail.com>
2021-08-02 22:59:52 +0900

    Refactor

--- ex1hellojpa/src/main/java/hellojpa/Member.java
+++ ex1hellojpa/src/main/java/hellojpa/Member.java
@@ -13,7 +13,34 @@ public class Member {
     @Column (name = "USERNAME")
     private String username;
 
-    @Column (name = "TEAM_ID")
-    private Long teamId;
+//    @Column (name = "TEAM_ID")
+//    private Long teamId;
 
+    @ManyToOne
+    @JoinColumn (name = "TEAM_ID")
+    private Team team;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
 }
