8d81643a99bc68b0e4bddc540206133d75bdd970
김민재 <joshuara7235@gmail.com>
2021-08-23 21:29:03 +0900

    Entity setting (member, team)

--- data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -1,24 +1,43 @@
 package study.datajpa.entity;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity //entity 를 쓰려면 기본생성자를 만들어야한다.
 @Getter
-@NoArgsConstructor
+@Setter
+@NoArgsConstructor (access = AccessLevel.PROTECTED)
+@ToString (of = {"id", "username","age"})
 public class Member {
 
     @Id @GeneratedValue
+    @Column (name = "member_id")
     private Long id;
     private String username;
+    private int age;
+
+    @ManyToOne (fetch = FetchType.LAZY)
+    @JoinColumn (name = "team_id")
+    private Team team;
 
     public Member(String username) {
         this.username = username;
     }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+    //연관관계 편의 메소드
+    public void changeTeam (Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+
 
 }
