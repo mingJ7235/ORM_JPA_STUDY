24cf4982579a39a91f1166492ef0eea7c8f4f03a
김민재 <joshuara7235@gmail.com>
2021-08-25 22:53:18 +0900

    Query -> for DTO -> apply for repository

--- data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package study.datajpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 
 import java.util.List;
@@ -21,4 +22,11 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     @Query("select m from Member m where m.username = :username and m.age = :age") // 쿼리 문법이 틀렸을 시, App 로딩시점에서 오류가 난다. 잡기 쉽다.
     List<Member> findUser (@Param("username") String username, @Param("age") int age);
 
+    @Query("select m.username from Member m ")
+    List<String> findUsernameList();
+
+    //DTO를 반환하려고할때는 이렇게 new operation을 통해서 반환시켜야한다.
+    //but, QueryDSL을 사용하면 이것도 편해진다.
+    @Query("select new study.datajpa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
+    List<MemberDto> findMemberDto();
 }
