50ae2d5d170e210da6a599520677590b029896a0
김민재 <joshuara7235@gmail.com>
2021-08-25 22:32:37 +0900

    @Query function of Spring data JPA

--- data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,6 +1,8 @@
 package study.datajpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import study.datajpa.entity.Member;
 
 import java.util.List;
@@ -16,7 +18,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     List<Member> findTop3By(); //limit 쿼리
 
-
-
+    @Query("select m from Member m where m.username = :username and m.age = :age") // 쿼리 문법이 틀렸을 시, App 로딩시점에서 오류가 난다. 잡기 쉽다.
+    List<Member> findUser (@Param("username") String username, @Param("age") int age);
 
 }
