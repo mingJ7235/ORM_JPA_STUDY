6b21b28f8fd23e6d825a98014b7c2667a0ce69b5
김민재 <joshuara7235@gmail.com>
2021-09-05 22:09:25 +0900

    Querydsl basic setting for running

--- QueryDslStudy/src/test/java/com/example/querydslstudy/QueryDslStudyApplicationTests.java
+++ QueryDslStudy/src/test/java/com/example/querydslstudy/QueryDslStudyApplicationTests.java
@@ -1,13 +1,43 @@
 package com.example.querydslstudy;
 
+import com.example.querydslstudy.entity.Hello;
+import com.example.querydslstudy.entity.QHello;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
+
 class QueryDslStudyApplicationTests {
+
+	@Autowired
+	//@PersistenceContext
+	EntityManager em;
 
 	@Test
 	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+//		QHello qHello = new QHello("h");
+		QHello qHello = QHello.hello;
+
+		Hello result = query
+				.selectFrom(qHello)
+				.fetchOne();
+
+		assertThat(result).isEqualTo(hello);
+		assertThat(result.getId()).isEqualTo(hello.getId());
 	}
 
 }
