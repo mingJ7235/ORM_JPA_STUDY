40a0560a1d79bc8c7d16f40d6a4cff8153245229
Minjae <joshuara7235@spiderkim.com>
2021-09-06 09:51:37 +0900

    Paging study

--- QueryDslStudy/src/test/java/com/example/querydslstudy/QuerydslBasicTest.java
+++ QueryDslStudy/src/test/java/com/example/querydslstudy/QuerydslBasicTest.java
@@ -138,7 +138,7 @@ public class QuerydslBasicTest {
      * 회원 정렬순서
      * 1. 회원 나이 내림차순 (desc)
      * 2. 회원 이름 올림차순 (asc)
-     * 단 2에서 회원 이름이 없으면 마지막에 출력 (null last 기능)
+     * 단 회원 이름이 없으면 마지막에 출력 (null last 기능)
      */
     @Test
     public void sort() {
@@ -159,6 +159,33 @@ public class QuerydslBasicTest {
         assertThat(member5.getUsername()).isEqualTo("member5");
         assertThat(member6.getUsername()).isEqualTo("member6");
         assertThat(memberNull.getUsername()).isNull();
+
+    }
+
+    @Test
+    public void paging1 () {
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .orderBy(member.username.desc())
+                .offset(1)
+                .limit(2)
+                .fetch();
+
+        assertThat(result.size()).isEqualTo(2);
+    }
+
+    @Test
+    public void paging2 () {
+        QueryResults<Member> queryResults = queryFactory
+                .selectFrom(member)
+                .orderBy(member.username.desc())
+                .offset(1)
+                .limit(2)
+                .fetchResults();
+        assertThat(queryResults.getTotal()).isEqualTo(4);
+        assertThat(queryResults.getLimit()).isEqualTo(2);
+        assertThat(queryResults.getOffset()).isEqualTo(1);
+        assertThat(queryResults.getResults().size()).isEqualTo(2);
 
     }
 
